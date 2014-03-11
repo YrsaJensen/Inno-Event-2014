d1bc0d933f29b37c7d866b02c89b1aebfee5d5e2
mide765 <mide.smile@gmail.com>
2014-03-11 12:18:03 +0100

    Added a login page and a second page.
    If your credentials are correct, you will get to the other screen.

--- Resources/app.js
+++ Resources/app.js
@@ -1,58 +1,82 @@
 var win1 = Titanium.UI.createWindow({
-	backgroundColor: 'white'
+	backgroundColor : 'white'
 });
- 
+
 var username = Titanium.UI.createTextField({
-    color:'#336699',
-    top:60,
-    left:10,
-    width:300,
-    height:40,
-    hintText:'Username',
-    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
-    returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
-    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+	color : '#336699',
+	top : 60,
+	left : 10,
+	width : 300,
+	height : 40,
+	hintText : 'Username',
+	keyboardType : Titanium.UI.KEYBOARD_DEFAULT,
+	returnKeyType : Titanium.UI.RETURNKEY_DEFAULT,
+	borderStyle : Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
 });
 win1.add(username);
- 
+
 var password = Titanium.UI.createTextField({
-    color:'#336699',
-    top:110,
-    left:10,
-    width:300,
-    height:40,
-    hintText:'Password',
-    passwordMask:true,
-    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
-    returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
-    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+	color : '#336699',
+	top : 110,
+	left : 10,
+	width : 300,
+	height : 40,
+	hintText : 'Password',
+	passwordMask : true,
+	keyboardType : Titanium.UI.KEYBOARD_DEFAULT,
+	returnKeyType : Titanium.UI.RETURNKEY_DEFAULT,
+	borderStyle : Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
 });
 win1.add(password);
- 
+
 var loginBtn = Titanium.UI.createButton({
-    title:'Login',
-    top:160,
-    width:90,
-    height:35,
-    borderRadius:1,
-    font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14}
+	title : 'Login',
+	top : 160,
+	width : 90,
+	height : 35,
+	borderRadius : 1,
+	font : {
+		fontFamily : 'Arial',
+		fontWeight : 'bold',
+		fontSize : 14
+	}
 });
 
+var credentials = [{
+	username : "nurse1",
+	password : "password1"
+}, {
+	username : "nurse2",
+	password : "password2"
+}, {
+	username : "nurse3",
+	password : "password3"
+}];
 
-var user = 'one';
-var pass = 'two';
+//credentials[++i] = [{}];
+//alert(credentials.length);
+//alert(credentials[0].username);
+//var user = 'one';
+//var pass = 'two';
 
-loginBtn.addEventListener('click',function(e)
-{
-    if (username.value != '' && password.value != '')
-    {
-        if (username.value == user && password.value == pass)
-        	alert("Welcome " + user + ". Your password is: " + pass);
-    }
-    else
-    {
-        alert("Username/Password are required");
-    }
+loginBtn.addEventListener('click', function(e) {
+	if (username.value != '' && password.value != '') {
+		for (var i = 0; i < 3; i++) {
+			if (username.value == credentials[i].username && password.value == credentials[i].password) {
+				alert("Welcome " + credentials[i].username + ". Your password is: " + credentials[i].password);
+				var win3 = Ti.UI.createWindow({
+					url : "window.js",
+					title : "newWindow"
+				});
+
+				Titanium.UI.currentWindow.open(win3, {
+					animated : true
+				});
+			}
+		}
+	} else {
+		alert("Username/Password are required");
+	}
 });
 
 win1.add(loginBtn);
